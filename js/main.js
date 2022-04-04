94079b738c8a5782b3e0b3215c92979a7284d017
Raghav Agarwal <raghav.agarwal@hexagon.com>
2022-04-03 20:31:34 -0500

    Added logic to set active class on selected nav item

--- js/main.js
+++ js/main.js
@@ -4,28 +4,68 @@ const menuBtn = document.querySelector( '.menu-btn' );
 const hamburger = document.querySelector( '.menu-btn_burger' );
 const nav = document.querySelector( '.nav' );
 var menuNav = null,
-    navItems = null;
+    navItems = null,
+    navLinks = null;
 
 const socialIcons = document.querySelector( '.social-icons' );
 await includeHtml('../pages/subpages/social-icons.html', socialIcons );
 
 let hiddenMenu = true;
-menuBtn.addEventListener( 'click', toggleMenu );
 
 
 async function loadSubPages() {
     try{
-        let result = await includeHtml('../pages/subpages/nav-options.html', nav );
+        await includeHtml('../pages/subpages/nav-options.html', nav );
         menuNav = await document.querySelector( '.menu-nav' );
         navItems = await document.querySelectorAll( '.menu-nav_item' );
-        menuBtn.addEventListener( 'click', toggleMenu );
+        navLinks = await document.querySelectorAll( '.menu-nav_link' );
     } catch( ex ){
         console.error( ex );
     }
 
 }
+ async function fireUp() {
+     try{
+         await loadSubPages();
+        var navActiveItem = getActiveNavItem();
+        setActiveLink( navActiveItem );
+          
+        menuBtn.addEventListener( 'click', toggleMenu );
+     }catch( ex ){
+         console.error( ex );
+     }
+ }
 
-loadSubPages();
+ fireUp();
+
+function  getActiveNavItem() {
+    for(var i = 0; i < navItems.length; i++) {
+        if( navItems[i].classList.contains( 'active' ) ) {
+            return navItems[i];
+        }
+    }
+    return null;
+}
+
+function setActiveLink( activeNavItem ) {
+    if( activeNavItem ) {
+        var index = selectedNavItemIndex();
+        if( index > -1 ) {
+            activeNavItem.classList.remove( 'active' );
+            navItems[index].classList.add( 'active' );
+        }
+
+    }
+}
+
+function selectedNavItemIndex(){
+    for( var i = 0; i < navLinks.length; i++ ) {
+        if( navLinks[i].href === window.location.href ) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 function toggleMenu() {
     if( hiddenMenu ) {
